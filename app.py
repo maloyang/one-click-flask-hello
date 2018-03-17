0388b325a96ba2d8042e02e15baba06a34ee55b7
maloyang <33815331+maloyang@users.noreply.github.com>
2018-03-17 22:12:16 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+from __future__ import unicode_literals
+
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def basic_url():
+    return 'hello'
+
+
+if __name__ == "__main__":
+    app.run()
